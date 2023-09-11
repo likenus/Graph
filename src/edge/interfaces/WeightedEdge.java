e16d34b974805c681c852142f8a97e57eb7deeca
likenus <linuskl26@gmail.com>
2023-09-11 12:14:18 +0200

    more javadoc

--- src/edge/interfaces/WeightedEdge.java
+++ src/edge/interfaces/WeightedEdge.java
@@ -2,7 +2,15 @@ package src.edge.interfaces;
 
 public interface WeightedEdge extends Edge {
     
+    /**
+     * Returns the weight value of this edge. Can be negative.
+     * @return An integer
+     */
     public int getWeight();
 
+    /**
+     * Sets the weight value for this edge. Value may be negative.
+     * @param weight The value to set the weight to
+     */
     public void setWeight(int weight);
 }
