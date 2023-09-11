57ecc635a823bf6b6c04a485b2cd9c34fac5217e
likenus <uxofb@student.kit.edu>
2023-09-11 02:16:49 +0200

    added some javadoc

--- src/edge/interfaces/Edge.java
+++ src/edge/interfaces/Edge.java
@@ -5,20 +5,30 @@ import src.vertices.Vertice;
 
 public interface Edge { 
     
+    /**
+     * Returns the other vertice connected to the edge.
+     * @param v
+     * @return
+     */
     Vertice getOther(Vertice v);
 
+    /**
+     * Returns a tuple containing both connected vertices. In directed edges, the tuple is ordered. 
+     * Undirected edges do not require an order, thus the returned order is the order of insertion.
+     * @return The two connected vertices
+     */
     Tuple<Vertice> getVertices();
 
     /**
      * Returns one of the connected vertices. On directed edges always returns the starting vertice. 
-     * On undirected edges this does not imply any direction. Use {@link getVertices} for undirected edges.
+     * On undirected edges this does not imply any direction. Use {@link Edge#getVertices} for undirected edges.
      * @return The starting vertice
      */
     Vertice start();
 
     /**
      * Returns one of the connected vertices. On directed edges always returns the ending vertice. 
-     * On undirected edges this does not imply any direction. Use {@link getVertices} for undirected edges.
+     * On undirected edges this does not imply any direction. Use {@link Edge#getVertices} for undirected edges.
      * @return The ending vertice
      */
     Vertice end();
