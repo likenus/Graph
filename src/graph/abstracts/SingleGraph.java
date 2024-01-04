5f1ea81c448eaca3d9b18ef252db1b310eaf5eee
Linus <linuskl26@gmail.com>
2024-01-04 23:43:59 +0100

    forgot something

--- src/graph/abstracts/SingleGraph.java
+++ src/graph/abstracts/SingleGraph.java
@@ -3,7 +3,6 @@ package src.graph.abstracts;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -92,7 +91,7 @@ public abstract class SingleGraph implements Graph {
             return this.neighbours(v);
         }
 
-        return null;
+        return new ArrayList<>();
     }
 
     @Override
