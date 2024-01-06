c88c9690ed8c0e23c50d8d4bed1d2995572fa212
ME4595 <68704617+ME4595@users.noreply.github.com>
2024-01-06 23:04:01 +0100

    Make LazyGraph even lazier (who needs to know the edges of a graph anyways?)

--- src/graph/graph/models/directed/LazyDirectedGraph.java
+++ src/graph/graph/models/directed/LazyDirectedGraph.java
@@ -1,8 +1,14 @@
 package src.graph.graph.models.directed;
 
+import src.graph.edge.interfaces.Edge;
+import src.graph.edge.models.DirectedEdge;
 import src.graph.vertices.interfaces.Vertice;
 import src.graph.vertices.models.Node;
 
+/**
+ * Lazy Graph for performance optimization of BFS. WARNING: This graph does not keep track if its edges, these are only
+ * accessible through the vertices.
+ */
 public class LazyDirectedGraph extends DirectedGraph {
 
     public LazyDirectedGraph(int startNode) {
@@ -23,5 +29,18 @@ public class LazyDirectedGraph extends DirectedGraph {
         }
 
         return addEdge(v, w, 1);
+    }
+
+    @Override
+    protected boolean addEdge(Vertice v, Vertice w, int value) {
+        if (v == null || w == null) {
+            return false;
+        }
+
+        Edge edge = new DirectedEdge(v, w, value);
+
+        v.connectEdge(edge);
+
+        return true;
     }
 }
